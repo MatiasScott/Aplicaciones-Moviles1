b1e2b61cc36740b83337e52ea29387abbd3a852a
MatiasScott <mvaldivieso31@gmail.com>
2021-09-06 21:17:29 -0500

    solucion home

--- proyectodecuarto/lib/Home.dart
+++ proyectodecuarto/lib/Home.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:proyectodecuarto/login.dart';
-import 'package:proyectodecuarto/main.dart';
-import 'probar.dart';
-
+import 'perfil.dart';
+import 'login.dart';
 import 'botones.dart';
+import 'servicio.dart';
 
 class Home extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -19,69 +18,61 @@ class Home extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-            child: null /* add child content here */,
           ),
           Container(
             // color: Colors.black,
-            margin: EdgeInsets.only(top: 35),
-            height: 60,
+            margin: EdgeInsets.only(top: 30),
+            height: 80,
             child: Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.only(left: 20),
-                  child: Icon(
-                    Icons.arrow_back_sharp,
-                    color: Colors.black,
-                    size: 40.0,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 30),
-                  alignment: Alignment.center,
-                  child: Text(
-                    '''CNEL APP''',
-                    style: TextStyle(
-                        color: Colors.blue.shade900,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 23),
-                  ),
-                ),
-                Container(
-                  width: 40,
-                  height: 40,
-                  margin: EdgeInsets.only(left: 70),
-                  alignment: Alignment.centerRight,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/img/cliente.png"),
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 30),
+                      alignment: Alignment.center,
+                      child: Text(
+                        '''CNEL APP''',
+                        style: TextStyle(
+                            color: Colors.blue.shade900,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 23),
                       ),
-                      shape: BoxShape.circle),
+                    ),
+                    Perfil(nombre: "Mateo"),
+                  ],
                 ),
+                /* ñaño aqui esta esta contenido la imagen del cliente y de los tres puntos*/
                 Container(
-                  height: 40,
-                  width: 40,
-                  alignment: Alignment.centerLeft,
-                  child: Icon(
-                    Icons.more_vert,
-                    color: Colors.blue.shade700,
-                    size: 40.0,
+                  margin: EdgeInsets.only(bottom: 40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 40,
+                        height: 40,
+                        margin: EdgeInsets.only(left: 70),
+                        alignment: Alignment.centerRight,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/img/cliente.png"),
+                            ),
+                            shape: BoxShape.circle),
+                      ),
+                      Container(
+                        height: 40,
+                        width: 40,
+                        alignment: Alignment.centerLeft,
+                        child: Icon(
+                          Icons.more_vert,
+                          color: Colors.blue.shade700,
+                          size: 40.0,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                /* Container(
-                  margin: EdgeInsets.only(left: 30,top: 55),
-                  alignment: Alignment.center,
-                  child: Text(
-                    '''MI NOMBRE''',
-                    style: TextStyle(
-                        color: Colors.blue.shade600,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15),
-                  ),
-                ),*/
               ],
             ),
           ),
@@ -96,8 +87,10 @@ class Home extends StatelessWidget {
                       heightC: 35,
                       widthC: 250,
                       onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Login()));
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Servicio()));
                       }),
                 ),
                 Container(
@@ -187,3 +180,4 @@ class Home extends StatelessWidget {
     );
   }
 }
+
